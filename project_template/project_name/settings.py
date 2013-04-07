b729f7ead08fa1139c1f19c49812c528a8047556
Han Xu <hgcrpd@gmail.com>
2013-04-07 23:09:43 +0800

    start over with the actual template files in Django

--- project_template/project_name/settings.py
+++ project_template/project_name/settings.py
@@ -1,4 +1,4 @@
-# Django settings for starterpack project.
+# Django settings for {{ project_name }} project.
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -78,7 +78,7 @@ STATICFILES_FINDERS = (
 )
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = 'jx!98@=u@vg1*zb6+9%uukk=g8m3ds%6_iy&amp;mfnr2ali1x0c&amp;^'
+SECRET_KEY = '{{ secret_key }}'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -97,10 +97,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'starterpack.urls'
+ROOT_URLCONF = '{{ project_name }}.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'starterpack.wsgi.application'
+WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
